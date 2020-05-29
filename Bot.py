df4d940aaff9e27361f42d3887d30d4a2c29f9ad
Marius Høgli Aasarød <44798981+marhaasa@users.noreply.github.com>
2020-05-29 12:07:46 +0200

    add 8ball

--- Bot.py
+++ Bot.py
@@ -1,4 +1,5 @@
 import discord
+import random
 import config
 
 from discord.ext import commands
@@ -11,7 +12,18 @@ async def on_ready():
 
 @client.command()
 async def ping(ctx):
-    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+    await ctx.send(f'Pong! Response time: {round(client.latency * 1000)}ms')
+
+@client.command(aliases=["8ball", "eightball", "Eightball"])
+async def _8ball(ctx, *, question):
+    response = [
+        "Yes.",
+        "No.",
+        "Maybe.",
+        "Absolutely not.",
+        "Shut up."
+    ]
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(response)}')
 
 
 client.run(config.bot_token)
